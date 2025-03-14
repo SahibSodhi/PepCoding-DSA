5bdd19c7bf224af3e24946e74f81aaa9dd6f3cb2
SahibSodhi <mssodhi11@gmail.com>
2025-03-14 23:45:14 +0530

    Implement solution for Pattern 7

---
+++ src/BasicsofDataStructures/Pattern7.java
@@ -0,0 +1,24 @@
+package BasicsofDataStructures;
+
+import java.util.Scanner;
+
+public class Pattern7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); 
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == j){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
